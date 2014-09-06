2948d08fd75405f7bedfa610caff8062091e2d5c
Austen Talbot <austentalbot@gmail.com>
2014-09-06 16:57:41 -0700

    Set up functionality to upload and send files

--- client/app.js
+++ client/app.js
@@ -1,6 +1,40 @@
-var app = angular.module('dataHero', []);
+var handleFileSelect = function(evt) {
+  var files = evt.target.files; // FileList object
 
-app.controller('load', function($http, $scope) {
-  $scope.query = function() {
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  var f;
+  for (var i = 0; i<files.length; i++) {
+    f = files[i];
+    output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                f.size, ' bytes, last modified: ',
+                f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                '</li>');
+  }
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+};
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+document.getElementById('files').addEventListener('change', function(evt) {
+  // console.log(evt.target.files[0]);
+  Papa.parse(evt.target.files[0], {
+  delimiter: ",",
+  header: false,
+  complete: function(results) {
+    console.log(results.data);
+    var data = results.data;
+    //multiple posts because of size limit
+    for (var i=0; i<10; i++) {
+      $.ajax('http://127.0.0.1:6474/firstUpload', {
+        type: 'POST',
+        data: data.slice(Math.floor(i*(data.length/10)), Math.floor((i+1)*(data.length/10)))
+      }).done(function(results) {
+        console.log(results);
+      });
+    }
+  }
+
   });
 });
+
+
--- server/app.js
+++ server/app.js
@@ -2,9 +2,36 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 6474;
+var cors=require('cors');
+var bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.get('/*', function(req, res){
+
+app.post('/firstUpload', function(req, res){
+  console.log('intercepted');
+  console.log(req);
+  res.status(200).send();
+});
+
+app.post('/secondUpload', function(req, res){
+  console.log('intercepted');
+  console.log(req);
+  res.status(200);
+});
+
+app.post('*', function(req, res){
+  console.log('intercepted');
+  console.log(req);
+  res.status(200);
+});
+
+app.get('*', function(req, res){
+  console.log('intercepted');
+  console.log(req);
   res.send('Hello world');
+  res.status(200);
 });
 
 var server = app.listen(port, function(){
