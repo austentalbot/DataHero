1be53863a856b97adddf394ec46067c118c0ce96
Austen Talbot <austentalbot@gmail.com>
2014-09-06 18:52:35 -0700

    Adjusting method of loading data

--- client/app.js
+++ client/app.js
@@ -8,23 +8,25 @@ var upload = function(file, calls, path) {
       var data = results.data;
       //multiple posts because of size limit
       for (var i=0; i<calls; i++) {
-        $.ajax('http://127.0.0.1:6474/'+path, {
-          type: 'GET',
-          // data: {'a': 'test', 'b': 'test'}
-          data: {observations: data.slice(Math.floor(i*(data.length/calls)), Math.floor((i+1)*(data.length/calls)))}
-        }).done(function(results) {
-          console.log(results);
-        });
+        (function(i) {
+          $.ajax('http://127.0.0.1:6474/'+path, {
+            type: 'GET',
+            data: {observations: data.slice(Math.round(i*(data.length/calls)), Math.round((i+1)*(data.length/calls)))}
+          }).done(function(results) {
+            console.log(results);
+          });
+        })(i);
       }
     }
   });
 }
 
 
-document.getElementById('employeeFile').addEventListener('change', function(evt) {
-  upload(evt.target.files[0], 3, 'employee');
-});
+// document.getElementById('employeeFile').addEventListener('change', function(evt) {
+//   upload(evt.target.files[0], 3, 'employee');
+// });
 
-document.getElementById('salaryFile').addEventListener('change', function(evt) {
-  upload(evt.target.files[0], 18, 'salary');
-});
+// document.getElementById('salaryFile').addEventListener('change', function(evt) {
+//   upload(evt.target.files[0], 18, 'salary');
+// });
+
--- server/app.js
+++ server/app.js
@@ -4,26 +4,74 @@ var app = express();
 var port = process.env.PORT || 6474;
 var cors=require('cors');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 
+//load credentials
+if (process.env.PORT===undefined) {
+  var credentials = require('./credentials.js');
+} else {
+  var credentials = {
+    dbHost: process.env['dbHost'],
+    dbUser: process.env['dbUser'],
+    dbPassword: process.env['dbPassword'],
+    database: process.env['database']
+  };
+}
 
-app.get('/employee', function(req, res){
+// var connection = mysql.createPool({
+//   connectionLimit: 4,
+//   host: credentials.dbHost,
+//   user: credentials.dbUser,
+//   password: credentials.dbPassword,
+//   database: credentials.database
+// });
+
+
+//create employee table
+// employee_id / birthdate / firstname / lastname / sex / start_date
+/*
+connection.query('CREATE TABLE employees ( \
+                employee_id int, \
+                birthday date, \
+                firstname varchar(50), \
+                lastname varchar(50), \
+                sex char(1), \
+                start_date date);', function(err, rows, fields) {
+  if (err) throw err;
+  console.log(rows);
+});
+*/
+
+// var employee = ["86679", "1964-07-09", "Chaitali", "Gargeya", "M", "1997-01-06"];
+// connection.query('SELECT * from employees;', function(err, rows, fields) {
+// // connection.query('INSERT INTO employees values (?, ?, ?, ?, ?, ?);', employee, function(err, rows, fields) {
+//   if (err) throw err;
+//   console.log(rows);
+// });
+
+
+
+app.post('/employee', function(req, res){
   console.log('intercepted employee');
-  console.log(req.query);
-  res.status(200).send();
+
+  res.status(200).send('<h3>Upload salary data</h3>' +
+    '<form action="http://127.0.0.1:6474/salary" enctype="multipart/form-data" method="post">' +
+      '<input type="file" name="upload" multiple="multiple"><br>' +
+      '<input type="submit" value="Upload">' +
+    '</form>');
 });
 
-app.get('/salary', function(req, res){
+app.post('/salary', function(req, res){
   console.log('intercepted salary');
-  console.log(req.query);
   res.status(200).send();
 });
 
 app.get('*', function(req, res){
   console.log('intercepted other');
-  console.log(req.body);
   res.status(200).send();
 });
 
